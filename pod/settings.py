f43f78a596e8193d5716131518757b5916abea96
Yekaterina Novikova <ykatesque@gmail.com>
2014-12-15 10:19:14 -0800

    experiment

--- pod/settings.py
+++ pod/settings.py
@@ -61,12 +61,13 @@ WSGI_APPLICATION = 'pod.wsgi.application'
 
 DATABASES = {
 'default': {
-'ENGINE': 'django.db.backends.postgresql_psycopg2',
-'NAME': 'portal',
-'USER': 'test',                      # Not used with sqlite3.
-        'PASSWORD': 'test',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    'NAME': 'portal',                      # Or path to database file if using sqlite3.
+    # The following settings are not used with sqlite3:
+    'USER': 'Ykat',
+    'PASSWORD': '',
+    'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+    'PORT': '',                      # Set to empty string for default.
 }
 }
 
@@ -88,7 +89,13 @@ USE_TZ = True
 
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+
+# DATABASES['default'] =  dj_database_url.config()
+
+# DATABASE_URL = 'postgresql:///postgresql'
+
+
+# DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
