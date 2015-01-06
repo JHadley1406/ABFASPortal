4cbc1a889799a10f16459b8d52bbf4b8e642cdb7
Yekaterina Novikova <ykatesque@gmail.com>
2015-01-05 16:17:55 -0800

    garlic

--- pod/settings.py
+++ pod/settings.py
@@ -61,9 +61,12 @@ WSGI_APPLICATION = 'pod.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+
+DATABASES = {'default': dj_database_url.config(default=os.environ["DATABASE_URL"])}
+
+
 DATABASES = {
-'default': dj_database_url.config(default=os.environ["DATABASE_URL"]),
-{
+'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
     'NAME': 'portal',                      # Or path to database file if using sqlite3.
     # The following settings are not used with sqlite3:
