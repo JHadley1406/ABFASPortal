e5e10b022368bf27d0dd0bdfebb92d146d061641
Josiah Hadley <josiah.hadley@gmail.com>
2015-01-15 15:07:46 -0800

    added unicode method to WhatsUpData, added single quotes to user_id foreign key object

--- whatsup/models.py
+++ whatsup/models.py
@@ -4,22 +4,18 @@ from jsonfield import JSONField
 
 # Create your models here.
 class WhatsUpData(models.Model):
-    user_id = models.ForeignKey(PodUser)
+    user_id = models.ForeignKey('PodUser')
     action_id = models.ForeignKey('WhatsUpActions')
     whats_up_text = models.TextField()
     created_on = models.DateTimeField(auto_now_add = True)
     last_updated = models.DateTimeField(auto_now_add = True)
+    json_data = JSONField(default={}, blank=True)
 
-<<<<<<< HEAD
     def __unicode__(self):
         return self.user_id.first_name + ": " + \
                self.action_id.action_text + " " + \
                self.whats_up_text
-=======
-    json_data = JSONField(default={}, blank=True)
 
-
->>>>>>> fa05dd3f832a33b11158f4b31ebc6b903aaa4baa
 
 class WhatsUpActions(models.Model):
     action_text = models.CharField(max_length = 32)
