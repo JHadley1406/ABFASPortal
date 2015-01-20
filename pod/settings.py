65992bf14297e2a7b2be7ee4cd3a628ad7143765
Yekaterina Novikova <ykatesque@gmail.com>
2015-01-20 15:45:10 -0800

    static

--- pod/settings.py
+++ pod/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
 'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
 'NAME': 'portal3',                      # Or path to database file if using sqlite3.
 # The following settings are not used with sqlite3:
-'USER': 'ykat',
+'USER': '',
 'PASSWORD': '',
 'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
 'PORT': '',                      # Set to empty string for default.
@@ -110,10 +110,10 @@ USE_TZ = True
 
 # DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
-import dj_database_url
-DATABASES = {
-   "default": dj_database_url.config(default='postgres://localhost'),
-}
+# import dj_database_url
+# DATABASES = {
+#    "default": dj_database_url.config(default='postgres://localhost'),
+# }
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -127,6 +127,8 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
+
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
